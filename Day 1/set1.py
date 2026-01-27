7b093f64be7c6ce25e71c47b75ddd5fc3e5d6079
DeveloperManikDas <developermanikdas@gmail.com>
2026-01-27 10:52:10 +0530

    day 1 set 1

---
+++ Day 1/set1.py
@@ -0,0 +1,104 @@
+#1 write a programe that aske the user to enter their name & age and prints in a sentencec. 
+
+# name = input("Whats your name? ")
+# age = int(input("What's your age? "))
+
+# print(f"Hey {name}, you are {age} years old.")
+
+
+
+#2 Take 2 numbers and print their sum, subtraction, multiplication, and quotient
+
+# num1 = int(input("What's your num1? "))
+# num2 = int(input("What's your num2? "))
+
+# addition = num1+num2
+# subtraction = num1-num2
+# multiply = num1*num2
+# quotient = num1/num2
+
+# print(f"The sum is {addition},  The subtraction is {subtraction}, The multiplication is {multiply} and quotient is {quotient}")
+
+
+
+#3 Ask the user to enter 2 ints and 1 float. print their int avg value
+
+# num1 = int(input("Enter num1 "))
+# num2 = int(input("Enter num2 "))
+# num3 = float(input("Enter num3 "))
+
+# average = float(num1+num2+num3)/3
+# print(average)
+
+
+
+#4 The user enters a string Containing a number convert it to An integer, a float and a string again
+
+# string = input("Enter number: ")
+
+# int_num = int(string)
+# float_num = float(string)
+# string_num = str(string)
+
+# print(f"{int_num} is a type of {type(int_num)}")
+# print(f"{int_num} is a type of {type(float_num)}")
+# print(f"{int_num} is a type of {type(string_num)}")
+
+
+
+#5 Evaluate this
+
+# x = 10+3*2**2 
+# print(x)
+
+#6 Write a programe to swap a value 
+
+# a = int(input("Enter a number: ")) 
+# b = int(input("Enter another number: ")) 
+
+# temp = a
+# a = b
+# b = temp
+
+# print(a,b)
+
+
+
+#7 Convert celsius temp in fahrenheit
+
+# celsius_temp = int(input("Enter temperature in Celsius: "))
+# fahrenheit_temp = celsius_temp*(9/5)+32
+
+# print(fahrenheit_temp)
+
+
+
+#8 area of circle
+
+# radius = int(input("Enter the radius: "))
+# area = (22/7)*radius**2
+
+# print(area)
+
+
+
+#9 Calculate simple interest
+
+# P = int(input("Enter principal amount: "))
+# R = int(input("Enter rate of interest : "))
+# T = int(input("Enter time period: "))
+# simple_interest = (P*R*T)/100
+
+# print(simple_interest)
+
+
+
+#10 Take decimal number as input and output the integer part and fraction part. 
+
+# n = float(input("Enter a decimal number: "))
+
+# integer_part = int(n)
+
+# fraction_part = n - integer_part
+
+# print(f"{integer_part}, {fraction_part:.2f}")
