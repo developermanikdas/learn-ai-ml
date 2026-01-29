44aabbfe16d6b290257ddf9eae63b0aa09469da4
DeveloperManikDas <developermanikdas@gmail.com>
2026-01-29 19:43:15 +0530

    upto 7

--- Day 3/set2.py
+++ Day 3/set2.py
@@ -23,11 +23,47 @@
 
 #5 sum of digits
 
-n = int(input("Enter number: "))
-sum = 0
-while n>0:
-    last_digit = n%10
-    sum+=last_digit
-    n = int(n/10)
+# n = int(input("Enter number: "))
+# sum = 0
+# while n>0:
+#     last_digit = n%10
+#     sum+=last_digit
+#     n = int(n/10)
     
-print(sum) 
+# print(sum) 
+
+
+
+
+#6 divisable by 3 & 5
+
+# n = int(input("Enter range: "))
+
+# for i in range(1, n+1):
+#     if i%15 == 0:
+#         print(i)
+
+
+
+#7 continuously print (+ve) or (-ve) until typed "Quit"
+
+
+# while True:
+#     n = (input("Enter number or ('quit' to exit) "))
+    
+#     if n == "quit":
+#         break
+#     try:
+#         # Convert once and store it in a variable
+#         num = float(n) 
+        
+#         if num > 0:
+#             print("(+ve)")
+#         elif num < 0:
+#             print("(-ve)")
+#         else:
+#             print("Zero is neither (+ve) nor (-ve)")
+            
+#     except ValueError:
+#         print("Invalid input! Please enter a number or 'quit'.")  
+            
