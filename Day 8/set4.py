b5942ddcd203609e28fc15bc294052047dd2b9ad
DeveloperManikDas <developermanikdas@gmail.com>
2026-02-03 12:48:08 +0530

    set 4 q3

--- Day 8/set4.py
+++ Day 8/set4.py
@@ -23,22 +23,64 @@
 
 #2 Class & Objects
 
-class Book:
-    def __init__(self, title, author, review):
-        self.title = title
-        self.author = author
-        self.review = review
+# class Book:
+#     def __init__(self, title, author, review):
+#         self.title = title
+#         self.author = author
+#         self.review = review
         
-    def add_review(self, new_review):
-        self.review.append(new_review)
+#     def add_review(self, new_review):
+#         self.review.append(new_review)
     
-    def count_review(self):
-        print(len(self.review))
+#     def count_review(self):
+#         print(len(self.review))
         
-    def show_review(self):
-        print(self.review)    
+#     def show_review(self):
+#         print(self.review)    
         
-book1 = Book("The Secret", "Rhonda Byrne", ["It Was superb", "Page Turner", "Really Secret inside"])            
+# book1 = Book("The Secret", "Rhonda Byrne", ["It Was superb", "Page Turner", "Really Secret inside"])            
 
 
-book1.show_review()
+# book1.show_review()
+
+
+
+#3 Encapsulation
+
+class Student:
+    def __init__(self, name, roll, mark):  
+        self.set_name(name)
+        self.set_roll(roll)
+        self.set_mark(mark)
+    
+    def set_name(self, name):
+        if not name and name.stripe() == "":
+            print("Name cannot be empty")
+        else:    
+            self._name = name    
+            
+    def set_roll(self, roll):        
+        if roll > 100 and self._roll < 0:
+            print("Roll must be within 1 - 100")
+        else:
+            self._roll = roll    
+            
+    def set_mark(self, mark):
+        if mark < 0:
+            print("Name cannot be negative")      
+        else:
+            self._mark = mark   
+            
+    def get_name(self):
+        return self._name
+    def get_roll(self):
+        return self._roll
+    def get_mark(self):
+        return self._mark          
+            
+s1 = Student("Manik", 12, 99)     
+
+print(s1._name, s1._roll, s1._mark)
+print(s1.get_name())       
+print(s1.get_roll())       
+print(s1.get_mark())       
