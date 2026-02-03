fb76a762dbb42463aefecd7d9950bbcfb32b8410
DeveloperManikDas <developermanikdas@gmail.com>
2026-02-03 09:21:33 +0530

    day 8 set 4 q1

---
+++ Day 8/set4.py
@@ -0,0 +1,23 @@
+#1 Crete Class & Objects
+
+# class BankAccount:
+#     def __init__(self, account_number, owner_name, balance):
+#         self.account_number = account_number
+#         self.owner_name = owner_name
+#         self.balance  = balance
+        
+#     def deposit(self, add_balance):
+#         self.balance = add_balance+self.balance
+    
+#     def withdraw(self, withdral_amount):    
+#          self.balance = self.balance -withdral_amount
+    
+#     def check_balance(self):     
+#         print(self.balance)
+
+# user1 = BankAccount(647011, "Manik", 20_000)
+# user1 = BankAccount(647011, "Manik", 20_000)
+# user1.check_balance()    
+
+
+
